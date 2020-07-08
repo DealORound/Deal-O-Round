80567f95a27ebf6ae5a35943d88966ff4460262e
MrCsabaToth <csaba.toth.us@gmail.com>
2020-07-08 13:23:40 -0700

    Adding background pattern: green gradient and white diamond shape

---
+++ deal_o_round/lib/background_painter.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+class BackgroundPainter extends CustomPainter {
+  Path _diamondShape(Canvas canvas, Size size) {
+    final padding = 10.0;
+    final halfHeight = (size.height - 2 * padding) / 2;
+    final verticalCenter = size.height / 2;
+    final horizontalCenter = size.width / 2;
+    return Path()
+      ..moveTo(horizontalCenter, padding)
+      ..lineTo(horizontalCenter + halfHeight, verticalCenter)
+      ..lineTo(horizontalCenter, size.height - padding)
+      ..lineTo(horizontalCenter - halfHeight, verticalCenter);
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final diamondPath = _diamondShape(canvas, size);
+    diamondPath.close();
+    final diamondStrokePaint = Paint()
+      ..color = Color(0xAAFFFFFF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5
+      ..isAntiAlias = true;
+    canvas.drawPath(diamondPath, diamondStrokePaint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
--- deal_o_round/lib/deal_o_round_app.dart
+++ deal_o_round/lib/deal_o_round_app.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'background_painter.dart';
 import 'home_page.dart';
 
 class DealORoundApp extends StatelessWidget {
+  RadialGradient _getBackgroundGradient() {
+    return RadialGradient(
+      center: const Alignment(0.5, 0.5),
+      radius: 1.0,
+      colors: [
+        Colors.black,
+        Colors.green,
+        Colors.lightGreen,
+        Colors.lightGreenAccent,
+      ],
+      stops: [0.0, 0.3, 0.7, 1.0],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,7 +27,13 @@ class DealORoundApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: Container(
+        decoration: BoxDecoration(gradient: _getBackgroundGradient()),
+        child: CustomPaint(
+          painter: BackgroundPainter(),
+          child: HomePage(),
+        ),
+      ),
     );
   }
 }
