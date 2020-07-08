3f90f301fd63c567f1a9cc098ebcd755739e770e
MrCsabaToth <csaba.toth.us@gmail.com>
2020-07-08 13:23:47 -0700

    Adding buttons

--- deal_o_round/lib/home_center.dart
+++ deal_o_round/lib/home_center.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeCenter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    const String aboutUrl = "http://dealoround.com/about.html";
+    const String helpUrl = "http://dealoround.com/help.html";
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -13,11 +16,64 @@ class HomeCenter extends StatelessWidget {
           children: <Widget>[
             Column(
               children: <Widget>[
-                RaisedButton.icon(onPressed: null, icon: null, label: null),
-                RaisedButton.icon(onPressed: null, icon: null, label: null),
-                RaisedButton.icon(onPressed: null, icon: null, label: null),
+                RaisedButton.icon(
+                  onPressed: null,
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  icon: Icon(Icons.person),
+                  label: Text("Login"),
+                ),
+                RaisedButton.icon(
+                  onPressed: null,
+                  icon: Icon(Icons.format_list_numbered),
+                  label: Text("Top Scores"),
+                ),
+                RaisedButton.icon(
+                  onPressed: () => {
+                    debugPrint('Play!')
+                  },
+                  icon: Icon(Icons.settings),
+                  label: Text("Settings"),
+                ),
               ],
-            )
+            ),
+            Column(
+              children: <Widget>[
+                RaisedButton.icon(
+                  onPressed: () => {
+                    debugPrint('Play!')
+                  },
+                  icon: Icon(Icons.play_arrow),
+                  label: Text("Play"),
+                ),
+                RaisedButton.icon(
+                  onPressed: () async => {
+                    if (await canLaunch(aboutUrl)) {
+                      launch(aboutUrl)
+                    } else {
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Cannot open URL'),
+                      ))
+                    }
+                  },
+                  icon: Icon(Icons.info),
+                  label: Text("About"),
+                ),
+                RaisedButton.icon(
+                  onPressed: () async => {
+                    if (await canLaunch(helpUrl)) {
+                      launch(helpUrl)
+                    } else {
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Cannot open URL'),
+                      ))
+                    }
+                  },
+                  icon: Icon(Icons.help),
+                  label: Text("Help"),
+                ),
+              ],
+            ),
           ],
         )
       ],
