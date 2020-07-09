7039fcf78844b73d289d720e5a19e7e86f2034fe
MrCsabaToth <csaba.toth.us@gmail.com>
2020-07-09 15:45:32 -0700

    Converting var to final

--- deal_o_round/lib/title_line.dart
+++ deal_o_round/lib/title_line.dart
@@ -7,13 +7,13 @@ class TitleLine extends StatelessWidget {
     final List<Widget> titleCharacters = [];
     final HomePageState state = HomePage.of(context);
     const title = "Deal-O-Round";
-    var rightNow = state.rightNow;
-    var animationTick = rightNow.second;
-    var prePreHighlight = animationTick % title.length;
-    var preHighlight = (animationTick + 1) % title.length;
-    var highlightIdx = (animationTick + 2) % title.length;
-    var postHighlight = (animationTick + 3) % title.length;
-    var postPostHighlight = (animationTick + 4) % title.length;
+    final rightNow = state.rightNow;
+    final animationTick = rightNow.second;
+    final prePreHighlight = animationTick % title.length;
+    final preHighlight = (animationTick + 1) % title.length;
+    final highlightIdx = (animationTick + 2) % title.length;
+    final postHighlight = (animationTick + 3) % title.length;
+    final postPostHighlight = (animationTick + 4) % title.length;
 
     title.split('').asMap().forEach((idx, char) => titleCharacters.add(
       Text(
