87139073de5e6f34cf4a04f5c17af254ea639ab5
MrCsabaToth <csaba.toth.us@gmail.com>
2020-07-09 13:36:31 -0700

    More elaborate title highlight rolling effect

--- deal_o_round/lib/title_line.dart
+++ deal_o_round/lib/title_line.dart
@@ -7,7 +7,13 @@ class TitleLine extends StatelessWidget {
     final List<Widget> titleCharacters = [];
     final HomePageState state = HomePage.of(context);
     const title = "Deal-O-Round";
-    var highlightIdx = state.rightNow.second % title.length;
+    var rightNow = state.rightNow;
+    var animationTick = rightNow.second;
+    var prePreHighlight = animationTick % title.length;
+    var preHighlight = (animationTick + 1) % title.length;
+    var highlightIdx = (animationTick + 2) % title.length;
+    var postHighlight = (animationTick + 3) % title.length;
+    var postPostHighlight = (animationTick + 4) % title.length;
 
     title.split('').asMap().forEach((idx, char) => titleCharacters.add(
       Text(
@@ -15,7 +21,13 @@ class TitleLine extends StatelessWidget {
         style: TextStyle(
           fontSize: 60,
           fontFamily: 'Musicals',
-          color: highlightIdx == idx ? Colors.lightGreenAccent : Colors.white
+          color: idx == highlightIdx ? Colors.white : (
+              (idx == preHighlight || idx == postHighlight) ?
+              Colors.lightGreenAccent : (
+                (idx == prePreHighlight || idx == postPostHighlight) ?
+                  Colors.lightGreen : Colors.green
+              )
+          )
         )
       )
     ));
