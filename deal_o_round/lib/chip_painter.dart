ff365dc9f04a318d3e8cf76d573649db89ae6dd6
MrCsabaToth <csaba.toth.us@gmail.com>
2020-07-09 15:46:13 -0700

    Will try to use custom background painter because our InkScape SVG cannot converted by fluttericons.

---
+++ deal_o_round/lib/chip_painter.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+import 'package:flutter/widgets.dart';
+
+class ChipPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final verticalCenter = size.height / 2;
+    final horizontalCenter = size.width / 2;
+    final center = Offset(horizontalCenter, verticalCenter);
+    final whiteFill = Paint()
+      ..color = Color(0xFFFFFFFF)
+      ..style = PaintingStyle.fill
+      ..isAntiAlias = true;
+    canvas.drawCircle(
+        center,
+        verticalCenter,
+        whiteFill
+    );
+
+    final blackStroke = Paint()
+      ..color = Color(0xFF000000)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1
+      ..isAntiAlias = true;
+    canvas.drawCircle(
+        center,
+        verticalCenter - 4,
+        blackStroke
+    );
+
+    final circleRect = Rect.fromCircle(center: center, radius: verticalCenter - 2);
+    final blueStroke = Paint()
+      ..color = Color(0xFF0000FF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4
+      ..isAntiAlias = true;
+    final sweepAngle = 2 * pi / 16;
+    final stepAngle = sweepAngle * 2;
+    final startAngle = sweepAngle * 1.5;
+    final stopAngle = stepAngle + 2 * pi;
+    for (var angle = startAngle; angle <= stopAngle; angle += stepAngle) {
+      canvas.drawArc(circleRect, angle, sweepAngle, false, blueStroke);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
