521f9c7f814ae4afd59f746baabe230118462911
MrCsabaToth <csaba.toth.us@gmail.com>
2020-07-10 17:38:54 -0700

    Adding ChipWidget and using it for the Left Example

---
+++ deal_o_round/lib/chip_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'chip_painter.dart';
+
+class ChipWidget extends StatelessWidget {
+  ChipWidget({
+    this.suit,
+    this.value
+  });
+
+  final String suit;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = (this.suit == 'C' || this.suit == 'S') ?
+      Colors.black : Colors.red;
+    final suitStyle = TextStyle(
+      fontSize: 45,
+      fontFamily: 'Cards',
+      color: textColor
+    );
+    final valueStyle = TextStyle(
+      fontSize: 45,
+      fontFamily: 'Noto-Serif',
+      fontWeight: FontWeight.w700,
+      color: textColor
+    );
+
+    final text = value.length > 1 ?
+      <Widget>[
+        Text('I', style: valueStyle),
+        Text(value[1], style: valueStyle),
+        Text(suit, style: suitStyle),
+      ] :
+      <Widget>[
+        Text(value, style: valueStyle),
+        Text(suit, style: suitStyle),
+      ];
+
+    return SizedBox(
+      width: 80,
+      height: 80,
+      child: CustomPaint(
+        painter: ChipPainter(),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: text
+            )
+          )
+        )
+      )
+    );
+  }
+}
--- deal_o_round/lib/left_example.dart
+++ deal_o_round/lib/left_example.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'chip_widget.dart';
+import 'home_page.dart';
 
 class LeftExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final HomePageState state = HomePage.of(context);
+    final highlightSecond = state.rightNow.second % 3 == 0;
+    final textStyle = TextStyle(
+      fontSize: 26,
+      fontFamily: 'Musicals',
+      color: highlightSecond ? Colors.lightGreenAccent: Colors.green
+    );
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text(
-          'Left!'
-        )
+        Row(
+          children: <Widget>[
+            ChipWidget(suit: '3', value: 'Q'),
+            ChipWidget(suit: 'C', value: '6')
+          ]
+        ),
+        Row(
+          children: <Widget>[
+            ChipWidget(suit: 'S', value: 'Q'),
+            ChipWidget(suit: '3', value: '10')
+          ]
+        ),
+        Row(
+          children: <Widget>[
+            ChipWidget(suit: 'C', value: 'Q'),
+            ChipWidget(suit: '4', value: '6')
+          ]
+        ),
+        SizedBox(height: 10),
+        Text("Full House!", style: textStyle)
       ]
     );
   }
