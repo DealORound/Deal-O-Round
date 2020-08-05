7dd47fa79072d9e72b492bb41af31f4cfcd04772
MrCsabaToth <csaba.toth.us@gmail.com>
2020-08-04 23:17:15 -0700

    Missing Key parameter for StatefulWidget constructor, and moving towards adding selection logic with Listener

--- deal_o_round/lib/game/chip_widget.dart
+++ deal_o_round/lib/game/chip_widget.dart
@@ -4,11 +4,10 @@ import '../game/logic/suit.dart';
 import '../game/logic/value.dart';
 import 'chip_painter.dart';
 import 'chip_selection_painter.dart';
-import 'game_page.dart';
 
 class ChipWidget extends StatefulWidget {
   final PlayCard card;
-  ChipWidget({this.card});
+  ChipWidget({Key key, this.card}): super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -20,22 +19,12 @@ class ChipWidgetState extends State<ChipWidget> {
   final PlayCard card;
   String _suit;
   String _value;
-  bool _selected = false;
-  bool _neighbor = false;
 
   ChipWidgetState({
     this.card
   }) {
     this._suit = suitCharacter(card.suit);
     this._value = valueCharacter(card.value);
-  }
-
-  toggleSelected() {
-    _selected = !_selected;
-  }
-
-  toggleNeighbor() {
-    _neighbor = !_neighbor;
   }
 
   @override
@@ -57,41 +46,20 @@ class ChipWidgetState extends State<ChipWidget> {
     return SizedBox(
       width: 80,
       height: 80,
-      child: GestureDetector(
-        onDoubleTap: null,
-        onTap: () {
-          DateTime now = DateTime.now();
-          debugPrint("onTap at $now!");
-          setState(() {
-            now = DateTime.now();
-            debugPrint("onTap setState1 at $now!");
-            toggleSelected();
-            (context as Element).markNeedsBuild();
-            final state = GamePage.of(context);
-            state.setState(() {
-              now = DateTime.now();
-              debugPrint("onTap setState2 at $now!");
-              state.incrementSelected();
-            });
-            // TODO: toggle board as well
-            // TODO: display hand
-          });
-        },
-        child: CustomPaint(
-          painter: ChipPainter(),
-          foregroundPainter: ChipSelectionPainter(
-            selected: _selected, neighbor: _neighbor),
-          child: Center(
-            child: RichText(
-              maxLines: 1,
-              overflow: TextOverflow.visible,
-              text: TextSpan(
-                text: _value,
-                style: valueStyle,
-                children: <TextSpan>[
-                  TextSpan(text: _suit, style: suitStyle)
-                ]
-              )
+      child: CustomPaint(
+        painter: ChipPainter(),
+        foregroundPainter: ChipSelectionPainter(
+          selected: card.selected, neighbor: card.neighbor),
+        child: Center(
+          child: RichText(
+            maxLines: 1,
+            overflow: TextOverflow.visible,
+            text: TextSpan(
+              text: _value,
+              style: valueStyle,
+              children: <TextSpan>[
+                TextSpan(text: _suit, style: suitStyle)
+              ]
             )
           )
         )
