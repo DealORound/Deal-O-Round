bf53317c2e29c703af560a01c43ce1e9b4263f93
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-04 14:57:24 -0700

    Braking apart removal + insertion animation into two separate rounds, accomodate new bottomless shoe, part of fixes #13

--- deal_o_round/lib/game/logic/board.dart
+++ deal_o_round/lib/game/logic/board.dart
@@ -1,8 +1,7 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import '../logic/suit.dart';
-import '../logic/value.dart';
 import '../chip_widget.dart';
 import '../game_page.dart';
 import 'game_constants.dart';
@@ -10,15 +9,12 @@ import 'play_card.dart';
 import 'shoe.dart';
 
 class Board {
-  static const SHOE_SIZE = 4;
-
-  final shoe = Shoe(includeJokers: true, deckCount: SHOE_SIZE);
+  final shoe = Shoe(includeJokers: true);
   List<List<PlayCard>> board;
   final BoardLayout layout;
   final indexes = Iterable<int>.generate(GameState.BOARD_SIZE).toList();
 
   Board({this.layout}) {
-    shoe.shuffleAll();
     board = indexes
         .map((i) => getRandomCards(GameState.BOARD_SIZE +
             (layout == BoardLayout.Hexagonal && i % 2 == 0 ? -1 : 0)))
@@ -48,37 +44,49 @@ class Board {
   }
 
   removeHand(List<GlobalKey<AnimatedListState>> listKeys, int animationDelay) {
-    // Step 1: bubble the selected cards to the top for removal
-    // And remove (animated) them from the AnimatedLists
+    // Step 1: Remove selected cards from the UI / AnimatedLists (animated way)
     for (var x in indexes) {
-      int displacement = 0;
-      int maxY = GameState.BOARD_SIZE +
+      final maxY = GameState.BOARD_SIZE +
           (layout == BoardLayout.Hexagonal && x % 2 == 0 ? -1 : 0);
       for (var y = maxY - 1; y >= 0; y--) {
         if (board[x][y].selected) {
           listKeys[x].currentState.removeItem(
               y, (context, animation) => buildItem(board[x][y], animation),
               duration: Duration(milliseconds: animationDelay));
-          displacement += 1;
-        } else if (displacement > 0) {
-          board[x][y + displacement] = board[x][y];
-          board[x][y] = PlayCard(suit: Suit.Invalid, value: Value.Invalid);
-          board[x][y].selected = true;
         }
       }
     }
-    // Step 2: deal new cards in place of the removed (selected) cards
-    // And insert (animated) them to the AnimatedLists
-    for (var x in indexes) {
-      int maxY = GameState.BOARD_SIZE +
-          (layout == BoardLayout.Hexagonal && x % 2 == 0 ? -1 : 0);
-      for (var y = 0; y < maxY; y++) {
-        if (board[x][y].selected) {
-          board[x][y] = shoe.dealCard();
-          listKeys[x].currentState.insertItem(y);
+
+    Timer(Duration(milliseconds: animationDelay), () {
+      // Step 2: Remove selected cards from board
+      for (var x in indexes) {
+        int displacement = 0;
+        final maxY = GameState.BOARD_SIZE +
+            (layout == BoardLayout.Hexagonal && x % 2 == 0 ? -1 : 0);
+        for (var y = maxY - 1; y >= 0; y--) {
+          if (board[x][y].selected) {
+            displacement += 1;
+          } else if (displacement > 0) {
+            final swap = board[x][y + displacement];
+            board[x][y + displacement] = board[x][y];
+            board[x][y] = swap;
+          }
         }
       }
-    }
+
+      // Step 3: deal new cards in place of the removed (selected) cards
+      // And insert (animated) them to the AnimatedLists
+      for (var x in indexes) {
+        int maxY = GameState.BOARD_SIZE +
+            (layout == BoardLayout.Hexagonal && x % 2 == 0 ? -1 : 0);
+        for (var y = 0; y < maxY; y++) {
+          if (board[x][y].selected) {
+            board[x][y] = shoe.dealCard();
+            listKeys[x].currentState.insertItem(y);
+          }
+        }
+      }
+    });
   }
 
   spin(List<GlobalKey<AnimatedListState>> listKeys, int animationDelay) {
@@ -90,6 +98,7 @@ class Board {
             Random().nextInt(
                 layout == BoardLayout.Hexagonal && i % 2 == 0 ? 3 : 4) +
             1);
+
     // Step 2: spin (slot machine type rotation) by the rotation amounts
     // Step 2.1: visual removal
     for (var x in indexes) {
@@ -99,11 +108,13 @@ class Board {
             duration: Duration(milliseconds: animationDelay));
       }
     }
+
     // Step 2.2: rearrange board
     for (var x in indexes) {
       final divider = board[x].length - spin[x];
       board[x] = board[x].sublist(divider) + board[x].sublist(0, divider);
     }
+
     // Step 2.3: re-insert removed cards
     for (var x in indexes) {
       for (var y = 0; y < spin[x]; y++) {
