12487dddd152bb2537038348625c95eb6149d31b
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-12 18:32:10 -0700

    Enable UI aid for debugging

--- deal_o_round/lib/main.dart
+++ deal_o_round/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 import 'services/preferences.dart';
 import 'services/sound.dart';
@@ -7,6 +9,9 @@ import 'deal_o_round_app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Paint.enableDithering = true;
+  if (kDebugMode) {
+    debugPaintSizeEnabled = true;
+  }
   await PreferencesUtils.registerService()
       .then((pref) => Get.put<SoundUtils>(SoundUtils(pref: pref)));
   runApp(DealORoundApp());
