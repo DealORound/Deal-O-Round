43016f049ed12c80e18c3d6ef8828125ceb87277
MrCsabaToth <csaba.toth.us@gmail.com>
2020-09-27 16:32:37 -0700

    Fixes #19: when calculating ship size also factor in width (it was dependent on the height only, and in larger aspect ratios it could cause problem)

--- deal_o_round/lib/services/size.dart
+++ deal_o_round/lib/services/size.dart
@@ -1,14 +1,13 @@
+import 'dart:math';
+
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 double chipSize(BuildContext context) {
   final size = context.mediaQuerySize;
-  final height = size.height - 11;
-  return height / 5; // ~80
+  return min((size.height - 11) / 5, (size.width - 11) / 10); // ~80
 }
 
 double chipRadius(BuildContext context) {
-  final size = context.mediaQuerySize;
-  final height = size.height - 11;
-  return height / 10; // ~40
+  return chipSize(context) / 2;
 }
